6df2cd542181f1b694b4cda9304ff4251dc789d4
Thomas Krijnen <t.krijnen@gmail.com>
2020-09-02 10:42:10 +0200

    Identify coplanar facets for style lookup

--- geobim.cpp
+++ geobim.cpp
@@ -356,7 +356,9 @@ struct global_execution_context : public execution_context {
 	typedef CGAL::Polyhedron_3<TreeKernel> TreeShapeType;
 	typedef CGAL::AABB_face_graph_triangle_primitive<TreeShapeType, CGAL::Default, CGAL::Tag_false> Primitive;
 	typedef CGAL::AABB_traits<TreeKernel, Primitive> AAbbTraits;
+	typedef typename AAbbTraits::Bounding_box Bounding_box;
 	typedef CGAL::AABB_tree<AAbbTraits> AAbbTree;
+	typedef typename AAbbTree::Primitive_id Primitive_id;
 
 	AAbbTree tree;
 
@@ -451,12 +453,42 @@ struct global_execution_context : public execution_context {
 		CGAL::Cartesian_converter<Kernel_,TreeKernel> converter;
 
 		for (auto &f : faces(input)) {
-			auto O = CGAL::centroid(
-				converter(f->facet_begin()->vertex()->point()),
-				converter(f->facet_begin()->next()->vertex()->point()),
-				converter(f->facet_begin()->next()->next()->vertex()->point())
+
+			auto f_c = CGAL::centroid(
+				f->facet_begin()->vertex()->point(),
+				f->facet_begin()->next()->vertex()->point(),
+				f->facet_begin()->next()->next()->vertex()->point()
 			);
-			auto pair = tree.closest_point_and_primitive(O);
+
+			auto f_c_ic = converter(f_c);
+
+#if 0
+			// Find co-planar facets from original IFC geometries within bounding box of
+			// nef-constructed facet. Filter based on facet normal and distance along normal.
+
+			std::list<Primitive_id> primitives, coplanar_primitives;
+			Bounding_box f_bbox = CGAL::Polygon_mesh_processing::face_bbox(f, input);
+			tree.all_intersected_primitives(f_bbox, std::back_inserter(primitives));
+
+			auto f_norm = CGAL::Polygon_mesh_processing::compute_face_normal(f, input);
+			auto f_norm_ic = converter(f_norm);
+
+			for (auto& p : primitives) {
+				auto p_norm_ic = CGAL::Polygon_mesh_processing::compute_face_normal(p.first, *p.second);
+				auto dot = std::abs(CGAL::to_double(f_norm_ic * p_norm_ic));
+				if (dot > 0.999) {
+					auto p_p0_ic = p.first->facet_begin()->vertex()->point();
+					auto d = std::abs(CGAL::to_double((p_p0_ic - f_c_ic) * p_norm_ic));
+					if (d < 0.001) {
+						coplanar_primitives.push_back(p);
+					}
+				}
+			}
+
+			std::cout << "bbox " << primitives.size() << " coplanar " << coplanar_primitives.size() << std::endl;
+#endif
+
+			auto pair = tree.closest_point_and_primitive(f_c_ic);
 			typename TreeShapeType::Face_handle F = pair.second.first;
 			auto it = facet_to_style.find(F);
 			if (it != facet_to_style.end()) {
